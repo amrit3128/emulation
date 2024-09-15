b33e141e12f03900ea2bb7cb7687f3d5a30a8c5d
Amritanshu Tripathy <tripathyamritanshu7@gmail.com>
2024-09-15 10:37:04 +0530

    not much  code herte

---
+++ main.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdlib.h>
+
+struct CPU{
+
+    using Byte = unsigned char;
+    using Word = unsigned short;
+
+    Word PC;
+
+};
+
+int main() {
+    std::cout << "Help me\n" ;
+    return 0;
+}
